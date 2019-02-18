a306b3ba99e4e9fe97d1dc6d9b723c1d420bb65b
Edmond Wang <ywang53@ebay.com>
2019-02-18 20:50:08 +0800

    add task

--- c1-if-else/if-else.py
+++ c1-if-else/if-else.py
@@ -1,5 +1,15 @@
 #!/bin/python3
 
+'''
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+'''
+
 N = int(input())
 
 if N % 2 != 0:
