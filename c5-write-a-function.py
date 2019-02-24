4529316af6348885d8a40b6ecb5650291f4e0c3b
EdmondWang <644568491@qq.com>
2019-02-24 14:43:25 +0800

    Refine case for leap year

--- c5-write-a-function.py
+++ c5-write-a-function.py
@@ -1,6 +1,9 @@
 '''
 Write a task to judge if input year is leap year or not
 '''
+'''
+Write a task to judge if input year is leap year or not
+'''
 def is_leap(year):
     leap = False
 
@@ -8,6 +11,8 @@ def is_leap(year):
         if year % 100 == 0:
             if year % 400 == 0:
                 leap = True
+        else:
+            leap = True
 
     return leap
 
